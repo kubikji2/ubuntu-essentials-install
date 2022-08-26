0bb36aea15de4f8576cb13d5705aa59481132304
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-26 22:47:27 +0200

    [3D-print] removing snap from openscad install, now installed from AppImage.

--- appinstall/3Dprint/openscad/install.sh
+++ appinstall/3Dprint/openscad/install.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-sudo snap install openscad
+# variable defining the location for the AppImage
+target="$HOME/.Apps"
+path=$target"/openscad"
+fn="OpenSCAD-2021.01-x86_64.AppImage"
+
+# make Apps directory
+mkdir -p $target
+mkdir -p $path
+
+# get the AppImage
+wget https://files.openscad.org/OpenSCAD-2021.01-x86_64.AppImage
+# make it AppImage runable
+chmod +x $fn
+# move it to the target location
+mv $fn $path
+# create a link to ~/.local/bin 
+ln -s $path"/"$fn $HOME"/openscad"
