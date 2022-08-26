e00ec50f87932f9a47d5eae6ae3758ed7d73b13b
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-26 23:50:25 +0200

    [communications-discord] adding alias.

--- appinstall/communications/discord/install.sh
+++ appinstall/communications/discord/install.sh
@@ -8,3 +8,5 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 XDG_DATA_DIRS+=":/var/lib/flatpak/exports/share:/home/kvant/.local/share/flatpak/exports/share"
 # install discord using 
 flatpak install flathub com.discordapp.Discord -y
+# setup alias
+echo "alias telegram='flatpak run com.discordapp.Discord'" >> $HOME/.bashrc
