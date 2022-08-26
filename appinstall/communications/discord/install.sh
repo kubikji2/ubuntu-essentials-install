30add7211589a60f2a564c7c75849c521d2f9362
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-26 23:26:49 +0200

    [communications] removing dependency on the snap for discord.

--- appinstall/communications/discord/install.sh
+++ appinstall/communications/discord/install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-sudo snap install discord
+# install flatpack
+sudo apt install flatpak -y
+# add repository
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# add flatpak to environment variables
+XDG_DATA_DIRS+=":/var/lib/flatpak/exports/share:/home/kvant/.local/share/flatpak/exports/share"
+# install discord using 
+flatpak install flathub com.discordapp.Discord -y
