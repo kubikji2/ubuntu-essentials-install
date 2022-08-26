aa5d0a2552e94f043bd82ff33fd7e03274fddf2e
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-26 22:48:07 +0200

    [3D-print] removing snap for prusa-slicer, now installed from AppImage.

--- appinstall/3Dprint/prusaslicer/install.sh
+++ appinstall/3Dprint/prusaslicer/install.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
-sudo snap install prusa-slicer
+target="$HOME/.Apps"
+path=$target"/prusa-slicer"
+
+# make Apps directory
+mkdir -p $target
+mkdir -p $path
+# download the appimage
+wget https://cdn.prusa3d.com/downloads/drivers/prusa3d_linux_2_4_2.zip
+# unzip it to
+unzip prusa3d_linux_2_4_2 -d $path
+# make both prusaslicers GTK versions runable
+chmod +x $path/PrusaSlicer*
+# make link to the .local/bin
+ln -s $path/PrusaSlicer-2.4.2+linux-x64-GTK3-202204251120.AppImage "$HOME/.local/bin/prusaslicer"
