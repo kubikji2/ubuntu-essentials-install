fc54892800913b02736e37bc7f517e12c5e21338
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-26 23:02:15 +0200

    [communication] removing dependency on snap, caprine now use AppImage from github.

--- appinstall/communications/caprine/install.sh
+++ appinstall/communications/caprine/install.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-sudo snap install caprine
+# variable defining the location for the AppImage
+target="$HOME/.Apps"
+path=$target"/caprine"
+fn="Caprine-2.56.1.AppImage"
+
+# make Apps directory
+mkdir -p $target
+mkdir -p $path
+
+# download the AppImage
+wget https://github.com/sindresorhus/caprine/releases/download/v2.56.1/Caprine-2.56.1.AppImage
+# make it AppImage runable
+chmod +x $fn
+# move it to the target location
+mv $fn $path
+# create a link to ~/.local/bin 
+ln -s $path"/"$fn $HOME"/.local/bin/caprine"
