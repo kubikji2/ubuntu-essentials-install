d0cdcee88a681f6c6e97d43c556e048e85a23bd5
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-26 23:53:10 +0200

    [communications-discord] removing apt installing flatpak since it is part of the main install script now.

--- appinstall/communications/discord/install.sh
+++ appinstall/communications/discord/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-# install flatpack
-sudo apt install flatpak -y
 # add repository
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # add flatpak to environment variables
