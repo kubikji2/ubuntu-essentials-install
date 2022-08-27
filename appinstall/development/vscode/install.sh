8f079c79b16816e5b3598dc23d83d924ad3430d8
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-27 11:04:32 +0200

    [development-vscode] removing snap installation, now using apt.

--- appinstall/development/vscode/install.sh
+++ appinstall/development/vscode/install.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-sudo snap install --classic code
+# based on: https://code.visualstudio.com/docs/setup/linux
+
+# install wget and gpg if not present
+sudo apt-get install wget gpg -y
+# get key or something
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+# clean up
+rm -f packages.microsoft.gpg
+# install additional stuff
+
+sudo apt install apt-transport-https
+# update dependencies
+sudo apt update
+# instlal code
+sudo apt install code
