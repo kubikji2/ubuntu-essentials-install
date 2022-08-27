78b198a11231587dfaef40aa21b4c6a6619f530a
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-27 11:41:21 +0200

    [media-gimp] adding comments to the gimp.

--- appinstall/media/gimp/install.sh
+++ appinstall/media/gimp/install.sh
@@ -3,6 +3,6 @@
 # install gimp using flatpak
 # NOTE: it is currently recommended way of installing the gimp, see: https://www.gimp.org/downloads/
 flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref -y
-# add entry to 
+# add entry to ~/.local/bin
 echo "flatpak run org.gimp.GIMP//stable" >> $HOME"/.local/bin/gimp"
 chmod +x $HOME"/.local/bin/gimp"
