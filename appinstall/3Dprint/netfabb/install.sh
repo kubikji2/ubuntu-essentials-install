ccea77720c6d8dc127769e5b9f3dfa2a76d118a9
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-27 23:27:44 +0200

    [3D-netfabb] adding icon and desktop entry to enable netfabb to be run from the ubuntu menu and to be add to favourites.

--- appinstall/3Dprint/netfabb/install.sh
+++ appinstall/3Dprint/netfabb/install.sh
@@ -1,7 +1,8 @@
 
 # variable defining the location for the AppImage
 target="$HOME/.Apps"
-path=$target"/netfabb"
+name="netfabb"
+path=$target"/"$name
 fn="netfabb-free_7.4.0_linux64.tar.gz"
 
 # make Apps directory
@@ -17,6 +18,19 @@ mv $path/*/* $path/
 # make netfabb runable
 chmod +x $path"/netfabb_free"
 # create a link to ~/.local/bin 
-ln -s $path"/netfabb_free" $HOME"/.local/bin/netfabb"
+ln -s $path"/netfabb_free" $HOME"/.local/bin/"$name
 # clean up
 rm $fn
+
+# coppy icon to the .App/ directory
+#cp icon.png $path
+# creating entry in the menu and enabling adding to favourite
+echo -e "[Desktop Entry]
+Version=2.4.2
+Type=Application
+Name=NetFabb free
+Comment=Free version of netfabb by Miro Hroncok
+TryExec=$HOME/.local/bin/$name
+Exec=$HOME/.local/bin/$name
+Icon=$path/netfabb.png
+Actions=Editor" > $HOME/.local/share/applications/$name.desktop
