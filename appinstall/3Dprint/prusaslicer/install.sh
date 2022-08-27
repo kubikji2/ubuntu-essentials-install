34631a2c1856f97084c128737fb96b2857b2cac7
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-27 23:24:30 +0200

    [3D-prusaslicer] adding icon and desktop entry to enable prusaslicer to be run from the ubuntu menu and to be add to favourites.

--- appinstall/3Dprint/prusaslicer/install.sh
+++ appinstall/3Dprint/prusaslicer/install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 target="$HOME/.Apps"
-path=$target"/prusaslicer"
+name="prusaslicer"
+path=$target"/"$name
 
 # make Apps directory
 mkdir -p $target
@@ -13,6 +14,19 @@ unzip prusa3d_linux_2_4_2 -d $path
 # make both prusaslicers GTK versions runable
 chmod +x $path/PrusaSlicer*
 # make link to the .local/bin
-ln -s $path/PrusaSlicer-2.4.2+linux-x64-GTK3-202204251120.AppImage "$HOME/.local/bin/prusaslicer"
+ln -s $path/PrusaSlicer-2.4.2+linux-x64-GTK3-202204251120.AppImage "$HOME/.local/bin/"$name
 # clean up
 rm prusa3d_linux_2_4_2.zip 
+
+# coppy icon to the .App/ directory
+cp icon.png $path 
+# creating entry in the menu and enabling adding to favourite
+echo -e "[Desktop Entry]
+Version=2.4.2
+Type=Application
+Name=PrusaSlicer
+Comment=Slicer by Pruse Research
+TryExec=$HOME/.local/bin/$name
+Exec=$HOME/.local/bin/$name
+Icon=$path/icon.png
+Actions=Editor" > $HOME/.local/share/applications/$name.desktop
