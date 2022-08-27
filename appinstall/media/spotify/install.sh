27d027b73efbe1a1828059d3f0a1650f619f23a7
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-27 11:52:26 +0200

    [media-spotify] removing snap installation, addinf flatpak installation including the entry for ~/.local/bin.

--- appinstall/media/spotify/install.sh
+++ appinstall/media/spotify/install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-sudo snap install spotify
+# install using flatpak
+flatpak install flathub com.spotify.Client -y
+# add entry to ~/.local/bin
+echo "flatpak run com.spotify.Client" >> $HOME"/.local/bin/spotify"
+chmod +x $HOME"/.local/bin/spotify"
