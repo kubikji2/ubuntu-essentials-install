1ddae785baf7dcdc9ce39f0cd67c1ed245a7796f
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-30 12:06:46 +0200

    [coms-slack] fixing issues with non-working flatpak, reverting to some stable commit.

--- appinstall/communications/slack/install.sh
+++ appinstall/communications/slack/install.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt install slack -y
+# install from flatpak
+flatpak install flathub com.slack.Slack -y
+# this fucking thing wasn't fixed since May 17 2022 FFS
+# see: https://github.com/flathub/com.slack.Slack/issues/168
+flatpak update --commit='e42dd12ad288509cb4c7f94c7a370f9c72f7ddf03b202fbe3c2a7c9f6979b249' com.slack.Slack
+
+# add entry to ~/.local/bin
+echo "flatpak run com.slack.Slack" >> $HOME"/.local/bin/slack"
+chmod +x $HOME"/.local/bin/slack"
