79a88ec79542cac13a6b10d785001d1a349f5ac6
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-30 12:07:26 +0200

    [coms-discord] improving comments.

--- appinstall/communications/discord/install.sh
+++ appinstall/communications/discord/install.sh
@@ -4,7 +4,7 @@
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # add flatpak to environment variables
 XDG_DATA_DIRS+=":/var/lib/flatpak/exports/share:/home/kvant/.local/share/flatpak/exports/share"
-# install discord using 
+# install discord using flatpak
 flatpak install flathub com.discordapp.Discord -y
 # add entry to ~/.local/bin
 echo "flatpak run com.discordapp.Discord" >> $HOME"/.local/bin/discord"
