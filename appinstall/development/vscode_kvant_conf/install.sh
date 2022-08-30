a5f54bdc89ddd2300d54951b5907d512a259eba9
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-30 16:04:14 +0200

    [install] adding path propagation into install scripts, vs code conf now works.

--- appinstall/development/vscode_kvant_conf/install.sh
+++ appinstall/development/vscode_kvant_conf/install.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-cat vscode-extensions.conf | xargs -L 1 code --install-extension
+path="."
+
+if [[ $# -eq 1 ]]
+then
+    path=$1
+fi
+
+cat $path"/vscode-extensions.conf" | xargs -L 1 code --install-extension
