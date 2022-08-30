65b0dd3bac69def6d48453baf4c03a70cc59fbbf
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-08-30 15:42:49 +0200

    [media] updating flatpak-based scripts by adding flathub.

--- appinstall/media/gimp/install.sh
+++ appinstall/media/gimp/install.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# add flathub repository
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# add flatpak to environment variables
+XDG_DATA_DIRS+=":/var/lib/flatpak/exports/share:/home/kvant/.local/share/flatpak/exports/share"
+
 # install gimp using flatpak
 # NOTE: it is currently recommended way of installing the gimp, see: https://www.gimp.org/downloads/
 flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref -y
+
 # add entry to ~/.local/bin
 echo "flatpak run org.gimp.GIMP//stable" >> $HOME"/.local/bin/gimp"
 chmod +x $HOME"/.local/bin/gimp"
--- appinstall/media/vlc/install.sh
+++ appinstall/media/vlc/install.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# add flathub repository
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# add flatpak to environment variables
+XDG_DATA_DIRS+=":/var/lib/flatpak/exports/share:/home/kvant/.local/share/flatpak/exports/share"
+
 # install using flatpak
 flatpak install flathub org.videolan.VLC -y
+
 # add entry to ~/.local/bin
 echo "flatpak run org.videolan.VLC" >> $HOME"/.local/bin/vlc"
 chmod +x $HOME"/.local/bin/vlc"
