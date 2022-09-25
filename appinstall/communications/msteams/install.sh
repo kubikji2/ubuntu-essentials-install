6e32bae3ab11ab3d79beee832a43bbc93c548ec0
Jiri Kvant Kubik <jiri.kub@gmail.com>
2022-09-25 19:28:03 +0200

    [comms-msteams] adding curl to installscript.

--- appinstall/communications/msteams/install.sh
+++ appinstall/communications/msteams/install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+# install possibly missing curl
+sudo apt install curl -y
+
 # based on:
 # https://pureinfotech.com/install-microsoft-teams-linux/
-
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
 sudo apt update
