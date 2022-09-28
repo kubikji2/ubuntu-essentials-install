0f071654377e737b34fa78c8c42e88ce2a56ba89
Jiri Kvant Kubik <jiri.kub@gmail.com>
2022-09-28 14:00:45 +0200

    [media-blender] adding blender 3D modeling software installation.

---
+++ appinstall/media/blender/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# add flathub repository
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# add flatpak to environment variables
+XDG_DATA_DIRS+=":/var/lib/flatpak/exports/share:/home/kvant/.local/share/flatpak/exports/share"
+
+# install using flatpak
+sudo flatpak install flathub org.blender.Blender -y
+
+# add entry to ~/.local/bin and make it executable
+echo "flatpak run org.blender.Blender" > $HOME"/.local/bin/blender"
+chmod +x $HOME"/.local/bin/blender"
