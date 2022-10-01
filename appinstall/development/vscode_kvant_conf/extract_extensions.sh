49c54aaef1e50820bff43a3f9d21e3a1fc8a398f
Jiri Kvant Kubik <jiri.kub@gmail.com>
2022-10-01 21:26:06 +0200

    [developement-vscode extentions] updating the scripts to actually install extentions, lol.

--- appinstall/development/vscode_kvant_conf/extract_extensions.sh
+++ appinstall/development/vscode_kvant_conf/extract_extensions.sh
@@ -4,4 +4,4 @@
 
 # extract extentions and save them to the tmp_extentions.conf file
 # NOTE: based on https://stackoverflow.com/questions/35773299/how-can-you-export-the-visual-studio-code-extension-list
-code --list-extensions | xargs -L 1 echo code --install-extension >> tmp-extention.conf
+code --list-extensions >> tmp-extensions.conf
--- appinstall/development/vscode_kvant_conf/install.sh
+++ appinstall/development/vscode_kvant_conf/install.sh
@@ -7,4 +7,9 @@ then
     path=$1
 fi
 
-cat $path"/vscode-extensions.conf" | xargs -L 1
+# based on:
+# https://stackoverflow.com/questions/58513266/how-to-install-multiple-extensions-in-vscode-using-command-line
+cat $path"/tmp-extensions.conf" | while read extension || [[ -n $extension ]];
+do
+  code --install-extension $extension --force
+done
