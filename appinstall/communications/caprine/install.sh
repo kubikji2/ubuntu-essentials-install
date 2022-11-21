dbfb8bbcbc0f707bc2f56ec3cc097c548b6bf426
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-11-21 09:46:13 +0100

    [comms-caprine] updating to new verison and making script more general and easier to update in the future.

--- appinstall/communications/caprine/install.sh
+++ appinstall/communications/caprine/install.sh
@@ -4,29 +4,31 @@
 target="$HOME/.Apps"
 name="caprine"
 path=$target"/"$name
-fn="Caprine-2.56.1.AppImage"
+version="2.57.0"
+fn="Caprine-$version.AppImage"
+fn_template="Caprine-*.AppImage"
 
 # make Apps directory
 mkdir -p $target
 mkdir -p $path
 
 # download the AppImage
-wget https://github.com/sindresorhus/caprine/releases/download/v2.56.1/Caprine-2.56.1.AppImage
+wget https://github.com/sindresorhus/caprine/releases/download/v$version/Caprine-$version.AppImage
 # make it AppImage runable
 chmod +x $fn
 # move it to the target location
 mv $fn $path
 # create a link to ~/.local/bin 
-ln -fs $path"/"$fn $HOME"/.local/bin/"$name
+ln -fs $path"/"$fn_template $HOME"/.local/bin/"$name
 
 # coppy icon to the .App/ directory
 cp icon.png $path 
 # creating entry in the menu and enabling adding to favourite
 echo -e "[Desktop Entry]
-Version=2021.01
+Version=$version
 Type=Application
 Name=Caprine
-Comment=free software application for creating solid 3D computer-aided design objects.
+Comment=Unofficial Facebook messagner app.
 TryExec=$HOME/.local/bin/$name
 Exec=$HOME/.local/bin/$name
 Icon=$path/icon.png
