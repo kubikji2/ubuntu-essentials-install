f5ad3c95a1a2465b7b3847a35107c9a25999fb94
Jiří Kvant Kubík <jiri.kub@gmail.com>
2022-11-21 09:45:43 +0100

    [3D-prusaslice] making install script more general and updating to new slicer version.

--- appinstall/3Dprint/prusaslicer/install.sh
+++ appinstall/3Dprint/prusaslicer/install.sh
@@ -3,20 +3,25 @@
 target="$HOME/.Apps"
 name="prusaslicer"
 path=$target"/"$name
+# MODIFY THIS UPON NEW RELEASE
+version="2_5_0"
+version_dots=${version//"_"/"."}
 
 # make Apps directory
 mkdir -p $target
+# erease possible previous installations
+rm -rf $path
 mkdir -p $path
 # download the appimage
-wget https://cdn.prusa3d.com/downloads/drivers/prusa3d_linux_2_4_2.zip
+wget https://cdn.prusa3d.com/downloads/drivers/prusa3d_linux_$version.zip
 # unzip it to
-unzip prusa3d_linux_2_4_2 -d $path
+unzip "prusa3d_linux_"$version -d $path
 # make both prusaslicers GTK versions runable
 chmod +x $path/PrusaSlicer*
 # make link to the .local/bin
-ln -fs $path/PrusaSlicer-2.4.2+linux-x64-GTK3-202204251120.AppImage "$HOME/.local/bin/"$name
+ln -fs $path/PrusaSlicer*linux-x64-GTK3*.AppImage "$HOME/.local/bin/"$name
 # clean up
-rm prusa3d_linux_2_4_2.zip 
+rm prusa3d_linux_$version.zip 
 
 # coppy icon to the .App/ directory
 cp icon.png $path 
