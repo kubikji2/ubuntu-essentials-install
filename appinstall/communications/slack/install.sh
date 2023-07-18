f7291f8960e5bbc9ca2374904279d7659301687f
Jiri 'Kvant' Kubik <jiri.kub@gmail.com>
2023-07-18 23:44:44 +0200

    [communication-slack] adding autoconfirm for commit check.

--- appinstall/communications/slack/install.sh
+++ appinstall/communications/slack/install.sh
@@ -9,7 +9,7 @@ XDG_DATA_DIRS+=":/var/lib/flatpak/exports/share:/home/kvant/.local/share/flatpak
 sudo flatpak install flathub com.slack.Slack -y
 # this fucking thing wasn't fixed since May 17 2022 FFS
 # see: https://github.com/flathub/com.slack.Slack/issues/168
-sudo flatpak update --commit='e42dd12ad288509cb4c7f94c7a370f9c72f7ddf03b202fbe3c2a7c9f6979b249' com.slack.Slack
+sudo flatpak update --commit='e42dd12ad288509cb4c7f94c7a370f9c72f7ddf03b202fbe3c2a7c9f6979b249' com.slack.Slack -y
 
 # add entry to ~/.local/bin
 echo "flatpak run com.slack.Slack" > $HOME"/.local/bin/slack"
