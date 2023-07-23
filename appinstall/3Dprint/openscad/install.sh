fd9c67e8792abf22ee6313bf77762b6be5823f6a
Jiri 'Kvant' Kubik <jiri.kub@gmail.com>
2023-07-23 20:11:04 +0200

    [3D-openscad] fixing issue with the missing icon.

--- appinstall/3Dprint/openscad/install.sh
+++ appinstall/3Dprint/openscad/install.sh
@@ -19,8 +19,14 @@ mv $fn $path
 # create a link to ~/.local/bin 
 ln -fs $path"/"$fn $HOME"/.local/bin/"$name
 
-# coppy icon to the .App/ directory
-cp icon.png $path 
+# adding icon
+# getting script absolute dir path
+# loosely based on: https://linuxhint.com/absolute-path-script-bash/
+script_path=$(realpath "${BASH_SOURCE:-$0}")
+script_dir=$(dirname $script_path)
+# copy icon to the .App/ directory
+cp $script_dir/icon.png $path
+
 # creating entry in the menu and enabling adding to favourite
 echo -e "[Desktop Entry]
 Version=2021.01
