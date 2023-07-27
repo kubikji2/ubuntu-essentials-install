59f99037fb87bec1a36ed8badf981aea6f6acb63
Jiří Kvant Kubík <jiri.kub@gmail.com>
2023-07-27 15:50:26 +0200

    [utils-etcher] updating install script and adding the icon.

--- appinstall/utils/etcher/install.sh
+++ appinstall/utils/etcher/install.sh
@@ -1,10 +1,41 @@
 #!/usr/bin/env bash
 
-# based on:
-# https://raspberrytips.com/install-balena-etcher-ubuntu/
+# variable defining the location for the AppImage
+target="$HOME/.Apps"
+name="etcher"
+path=$target"/"$name
+version="1.18.11"
+fn="balenaEtcher-$version-x64.AppImage"
+fn_template="balenaEtcher-*.AppImage"
 
-curl -1sLf 'https://dl.cloudsmith.io/public/balena/etcher/setup.deb.sh' | sudo -E bash
+# make Apps directory
+mkdir -p $target
+mkdir -p $path
 
-sudo apt update
+# download the AppImage
+wget https://github.com/balena-io/etcher/releases/download/v$version/balenaEtcher-$version-x64.AppImage
+# make it AppImage runable
+chmod +x $fn
+# move it to the target location
+mv $fn $path
+# create a link to ~/.local/bin 
+ln -fs $path"/"$fn_template $HOME"/.local/bin/"$name
 
-sudo apt install balena-etcher-electron -y
+# adding icon
+# getting script absolute dir path
+# loosely based on: https://linuxhint.com/absolute-path-script-bash/
+script_path=$(realpath "${BASH_SOURCE:-$0}")
+script_dir=$(dirname $script_path)
+# copy icon to the .App/ directory
+cp $script_dir/icon.png $path
+
+# creating entry in the menu and enabling adding to favourite
+echo -e "[Desktop Entry]
+Version=$version
+Type=Application
+Name=Etcher
+Comment=Flash OS images to SD cards and USB drives, safely and easily.
+TryExec=$HOME/.local/bin/$name
+Exec=$HOME/.local/bin/$name
+Icon=$path/icon.png
+Actions=Editor" > $HOME/.local/share/applications/$name.desktop
