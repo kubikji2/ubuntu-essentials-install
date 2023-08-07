b4e477554752ef8a9635b73782dcd1a78430b7fb
Jiří Kvant Kubík <jiri.kub@gmail.com>
2023-08-07 13:14:05 +0200

    [utils] adding the unetbootin.

---
+++ appinstall/utils/unetbootin/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# based on the instruction at
+# https://unetbootin.github.io/linux_download.html
+
+sudo add-apt-repository ppa:gezakovacs/ppa
+sudo apt-get update
+sudo apt-get install unetbootin
+
