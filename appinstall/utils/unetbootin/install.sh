7c171adaecfdf22bb11fa52eff3da87c0f44202a
Jiri 'Kvant' Kubik <jiri.kub@gmail.com>
2023-08-19 11:36:40 +0200

    [utils-unetbootin] adding the auto-confirm option for the unetbooting install script.

--- appinstall/utils/unetbootin/install.sh
+++ appinstall/utils/unetbootin/install.sh
@@ -3,7 +3,7 @@
 # based on the instruction at
 # https://unetbootin.github.io/linux_download.html
 
-sudo add-apt-repository ppa:gezakovacs/ppa
-sudo apt-get update
-sudo apt-get install unetbootin
+sudo add-apt-repository ppa:gezakovacs/ppa -y
+sudo apt-get update -y
+sudo apt-get install unetbootin -y
 
