a2827ac58c693150f5d6e1013b6b4e9f5c8f88fe
Jiří Kvant Kubík <jiri.kub@gmail.com>
2023-11-08 11:26:00 +0100

    [3Dprint-meshlab] adding meshlab installation.

---
+++ appinstall/3Dprint/meshlab/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# add flathub repository
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# add flatpak to environment variables
+XDG_DATA_DIRS+=":/var/lib/flatpak/exports/share:/home/kvant/.local/share/flatpak/exports/share"
+
+# install freecad using flatpak
+flatpak install flathub net.meshlab.MeshLab -y
+
+# add entry to ~/.local/bin
+echo "flatpak run net.meshlab.MeshLab" > $HOME"/.local/bin/meshlab"
+chmod +x $HOME"/.local/bin/meshlab"
