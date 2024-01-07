18c8d87b9f96c528079810852978f7e3d7cbc5ee
Jiri 'Kvant' Kubik <jiri.kub@gmail.com>
2024-01-07 21:19:55 +0100

    [devel-godot] adding godot install script.

---
+++ appinstall/development/godot/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+target="$HOME/.Apps"
+name="godot"
+# MODIFY THIS UPON NEW RELEASE
+version="4_2_1"
+version_dots=${version//"_"/"."}
+path=$target"/"$name$version_dots
+filename=Godot_v$version_dots-stable_linux.x86_64
+archive_name=$filename.zip
+
+
+# make Apps directory
+mkdir -p $target
+# erease possible previous installations
+rm -rf $path
+mkdir -p $path
+# download the appimage
+wget https://github.com/godotengine/godot/releases/download/$version_dots-stable/$archive_name
+# unzip it to
+unzip -j $archive_name -d $path
+# make link to the .local/bin
+ln -fs $path/$filename "$HOME/.local/bin/"$name
+# clean up
+rm $archive_name
