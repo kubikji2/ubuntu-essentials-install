b03365e3616707f816871342f104984a7a990461
Jiri 'Kvant' Kubik <jiri.kub@gmail.com>
2024-12-31 20:03:07 +0100

    [dev] improving dotnet install.

--- appinstall/development/dotnet_sdk/install.sh
+++ appinstall/development/dotnet_sdk/install.sh
@@ -33,5 +33,21 @@ ubuntu_release=`echo "$ubuntu_release" | awk '{print $NF}'`
 wget https://packages.microsoft.com/config/ubuntu/$ubuntu_release/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
+
+# decrease priority of ubuntu repositories when using the dot net
+# based on: 
+# https://stackoverflow.com/questions/73753672/a-fatal-error-occurred-the-folder-usr-share-dotnet-host-fxr-does-not-exist
+tmp_pref_file="tmp.pref"
+echo -e "Package: dotnet* aspnet* netstandard*" >> $tmp_pref_file
+echo -e "Pin: origin "archive.ubuntu.com"" >> $tmp_pref_file
+echo -e "Pin-Priority: -10" >> $tmp_pref_file
+echo -e "" >> $tmp_pref_file
+echo -e "Package: dotnet* aspnet* netstandard*" >> $tmp_pref_file
+echo -e "Pin: origin "security.ubuntu.com"" >> $tmp_pref_file
+echo -e "Pin-Priority: -10" >> $tmp_pref_file
+
+sudo mv $tmp_pref_file "/etc/apt/preferences.d/99microsoft-dotnet.pref"
+
+
 # install Dotnet
-sudo apt-get update &&  sudo apt-get install -y dotnet-sdk-6.0
+sudo apt-get update &&  sudo apt-get install -y dotnet-sdk-7.0
